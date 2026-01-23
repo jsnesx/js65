41c52653c853eec86f76a31ee4ea79a3fc00fec3
James Rowe <jroweboy@gmail.com>
2026-01-23 00:23:15 -0500

    Update the Browser integration to work with the new action impl

--- integrations/dotnet/js65/BrowserEngine.cs
+++ integrations/dotnet/js65/BrowserEngine.cs
@@ -17,13 +17,18 @@ internal partial class AssmeblerContext : JsonSerializerContext;
 public partial class BrowserJsEngine(Js65Options? options = null, Js65Callbacks? callbacks = null)
     : Assembler(options, callbacks)
 {
-    [JSImport("compile", "js65.libassembler.js")]
+    [JSImport("compileActionsBrowser", "js65.libassembler.js")]
     [return: JSMarshalAs<JSType.Promise<JSType.String>>]
-    private static partial Task<string> Compile(string asm, string rom, string options,
+    private static partial Task<string> CompileActionsBrowser(
+        string modulesJson,
+        string assemblerOptsJson,
+        string linkerOptsJson,
+        string outputFormat,
         [JSMarshalAs<JSType.Function<JSType.String,JSType.String,JSType.String>>]
         Func<string, string, string> textCallback,
         [JSMarshalAs<JSType.Function<JSType.String,JSType.String,JSType.String>>]
-        Func<string, string, string> binaryCallback
+        Func<string, string, string> binaryCallback,
+        bool useSourceContents
     );
 
     private readonly Task<JSObject> _module = JSHost.ImportAsync("js65.libassembler.js", "js65/libassembler.js");
@@ -32,12 +37,37 @@ public partial class BrowserJsEngine(Js65Options? options = null, Js65Callbacks?
     {
         // Import the module and wait for it to finish
         _ = await _module;
+
         var modulesJson = SerializeModulesToJson();
-        var optsJson = JsonSerializer.Serialize(Options, AssmeblerContext.Default.Js65Options);
-        var b64Bytes = Convert.ToBase64String(rom);
-        var output = await Compile(modulesJson, b64Bytes, optsJson,
+
+        // Construct assembler options
+        var assemblerOpts = new
+        {
+            includePaths = Options.includePaths,
+            lineContinuations = Options.lineContinuations,
+            numberSeparators = Options.numberSeperators,
+            generateDebugInfo = Options.generateDebugInfo
+        };
+        var assemblerOptsJson = JsonSerializer.Serialize(assemblerOpts);
+
+        // Construct linker options
+        var linkerOpts = new
+        {
+            baseRom = Convert.ToBase64String(rom),
+            debugLevel = Options.debugLevel
+        };
+        var linkerOptsJson = JsonSerializer.Serialize(linkerOpts);
+
+        var output = await CompileActionsBrowser(
+            modulesJson,
+            assemblerOptsJson,
+            linkerOptsJson,
+            "binary",
             (basePath, filePath) => Callbacks?.OnFileReadText?.Invoke(basePath, filePath) ?? "",
-            (basePath, filePath) => Convert.ToBase64String(Callbacks?.OnFileReadBinary?.Invoke(basePath, filePath) ?? []));
+            (basePath, filePath) => Convert.ToBase64String(Callbacks?.OnFileReadBinary?.Invoke(basePath, filePath) ?? []),
+            Options.generateDebugInfo
+        );
+
         return JsonSerializer.Deserialize(Convert.FromBase64String(output), AssmeblerContext.Default.Js65CompileResult);
     }
 
